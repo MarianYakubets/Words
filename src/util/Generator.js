33433ef93cb2190cdcff6bc9ec98baee14bb8c6c
MarianYakubets <marian.yakubets@gmail.com>
2015-10-11 19:46:41 +0300

    h

--- src/util/Generator.js
+++ src/util/Generator.js
@@ -2,7 +2,7 @@ function Generator() {
 
 }
 Generator.generateLevel = function (words) {
-	var cells = [[]];
+
 	var summ = 0;
 	for (var i = 0; i < words.length; i++) {
 		summ = summ + words[i].length;
@@ -10,15 +10,40 @@ Generator.generateLevel = function (words) {
 	var width = Math.floor(Math.sqrt(summ));
 	var heigth = summ / width;
 
-	alert(Generator.getRandomNumber(11, 21));
+	var map = new Words.CellMap(width, heigth);
+
+	alert(Generator.getEmptyArea(0, 0, map));
 };
 
 Generator.getRandomNumber = function (min, max) {
 	return (Math.round(Math.random() * (max - min)) + min);
 };
 
-Generator.getEmptyAreas = function (cells) {
+Generator.getEmptyAreas = function (map) {
 };
+
+Generator.getEmptyArea = function (x, y, map) {
+	if (map.getCell(x, y) == "") {
+		var area = [];
+		area.push([x, y]);
+		area.push(Generator.getCellWithNeightbors(x, y, map));
+		return area;
+	}
+};
+
+Generator.getCellWithNeightbors = function (x, y, map) {
+	if (map.getCell(x, y) == "") {
+		map.setCell(x, y, "0")
+		var cells = [];
+		cells.push([x, y]);
+		cells.push(Generator.getCellWithNeightbors(x + 1, y, map));
+		cells.push(Generator.getCellWithNeightbors(x - 1, y, map));
+		cells.push(Generator.getCellWithNeightbors(x, y + 1, map));
+		cells.push(Generator.getCellWithNeightbors(x, y - 1, map));
+
+		return cells;
+	}
+}
 
 Generator.getSortedArray = function (arr, appending) {
 };
